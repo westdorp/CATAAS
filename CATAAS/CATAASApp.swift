9dea5f6e3ff0266f0239144aa1bb86b3128eda70
westdorp <34314700+westdorp@users.noreply.github.com>
2022-11-15 15:00:31 -0800

    Initial Commit

---
+++ CATAAS/CATAASApp.swift
@@ -0,0 +1,17 @@
+//
+//  CATAASApp.swift
+//  CATAAS
+//
+//  Created by  ¯\_(ツ)_/¯ on 11/15/22.
+//
+
+import SwiftUI
+
+@main
+struct CATAASApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
